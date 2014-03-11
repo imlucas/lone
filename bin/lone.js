da5cbf9d5a1d045514aae3820f58ea5ae4730234
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-03-11 12:12:44 -0400

    can run actions individually

--- bin/lone.js
+++ bin/lone.js
@@ -5,13 +5,13 @@ var fs = require('fs'),
   yargs = require('yargs')
     .usage([
       'Turn your node.js app into a single file executable.',
-      'Usage: lone [src]\n',
-      'psa: use npm run-script, save a kitten:\n',
-      '```',
-      'cd ~/my-app && npm install --save-dev jsontool lone && \\',
-      '`npm bin`/json -I -f package.json -c \'this.scripts.build="lone"\' && \\',
-      'npm run-script build;',
-      '```'
+      'Usage: lone [action]\n',
+      // '```',
+      // 'cd ~/my-app && npm install --save-dev jsontool lone && \\',
+      // '`npm bin`/json -I -f package.json -c \'this.scripts.build="lone"\' && \\',
+      // 'npm run-script build;',
+      // '```',
+      'where [action] is any one of: ' + lone.actions.join(', ')
     ].join('\n'))
     .options(lone.options)
     .demand(0);
@@ -20,7 +20,14 @@ if(yargs.argv.h) return yargs.showHelp();
 
 if(yargs.argv.setup) return console.error(fs.readFileSync(__dirname + '/setup', 'utf-8'));
 
-lone(yargs.argv._[0], yargs.argv, function(err, conf){
+var action = yargs.argv._[0] || 'all';
+
+if(lone.actions.indexOf(action) === -1){
+  console.error('unknown action `'+action+'`');
+  return yargs.showHelp();
+}
+
+lone[action](yargs.argv, function(err, conf){
   if(err) return console.error(':(', err.message) && process.exit(1);
   console.log(conf.out);
 });
