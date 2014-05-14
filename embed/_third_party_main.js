00abf5762cfe029d5b2cfb36b170f8164baf84d3
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-05-14 16:06:22 -0400

    [#10} hot on the trail

--- embed/_third_party_main.js
+++ embed/_third_party_main.js
@@ -13,8 +13,17 @@ catch(e){
   embedded = true;
 }
 
+process.on('uncaughtException', function(err) {
+  console.log('lone: caught exception: ', err);
+});
+
+process.on('exit', function(code){
+  console.log('lone: process exit', arguments);
+});
+
 function Lone(src, fn){
   this.src = src;
+  console.log('lone: init');
   console.log('lone: src is', this.src);
   this.dest = path.join(os.tmpDir(), path.basename(this.src.replace('.exe', '')));
   if(isWindows){
@@ -33,18 +42,52 @@ function Lone(src, fn){
 
       self.pkg = JSON.parse(data);
       console.log('lone: package data', self.pkg);
-      var Module = require('module');
-      console.log('lone: loading module', path.resolve(self.dest, self.pkg.bin[self.pkg.name]));
-      try{
-        Module._load(path.resolve(self.dest, self.pkg.bin[self.pkg.name]), null, true);
-        fn(null, self);
+      console.log('lone: argv[1] is', process.argv[1]);
+      if (process.argv[1] == 'debug') {
+        // Start the debugger agent
+        console.log('lone: starting debugger');
+        var d = require('_debugger');
+        d.start();
+      } else if (process.argv[1]) {
+        // make process.argv[1] into a full path
+        var path = require('path');
+        process.argv[1] = path.resolve(process.argv[1]);
+        if (process.env.NODE_UNIQUE_ID) {
+          console.log('lone: setup cluster worker');
+          var cluster = require('cluster');
+          cluster._setupWorker();
+          delete process.env.NODE_UNIQUE_ID;
+        }
+
+        if (global.v8debug && process.execArgv.some(function(arg) {
+              return arg.match(/^--debug-brk(=[0-9]*)?$/);
+            })) {
+          var debugTimeout = +process.env.NODE_DEBUG_TIMEOUT || 50;
+          return setTimeout(main, debugTimeout);
+        }
+        return main();
+      } else {
+        main();
       }
-      catch(e){
-        console.error('lone: failed to load module!', e);
-        process.exit(1);
-      }
+      console.log('lone: calling _tickCallback');
+      process._tickCallback();
+      fn(null, self);
     });
   });
+}
+
+function main(){
+  var Module = require('module'),
+    src = process.argv[1] ? process.argv[1] : path.resolve(self.dest, self.pkg.bin[self.pkg.name]);
+
+  console.log('lone: loading module', src);
+  try{
+    Module._load(src, null, true);
+  }
+  catch(e){
+    console.error('lone: failed to load module!', e);
+    process.exit(1);
+  }
 }
 
 // find a buffer from `this.src` that looks like a zip file,
