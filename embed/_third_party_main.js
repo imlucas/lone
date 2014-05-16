4657d6cfd07d01347de0a29408101663ca3f09c3
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-05-16 13:48:03 -0400

    [#10] clean out logging

--- embed/_third_party_main.js
+++ embed/_third_party_main.js
@@ -13,39 +13,22 @@ catch(e){
   embedded = true;
 }
 
-process.on('uncaughtException', function(err) {
-  console.log('lone: caught exception: ', err);
-});
-
-process.on('exit', function(code){
-  console.log('lone: process exit', arguments);
-});
-
 function Lone(src, fn){
   this.src = src;
-  console.log('lone: init');
-  console.log('lone: src is', this.src);
   this.dest = path.join(os.tmpDir(), path.basename(this.src.replace('.exe', '')));
   if(isWindows){
     this.dest = this.dest.replace('Temp\\', '');
   }
-  console.log('lone: dest is', this.dest);
   this.pkg = {};
   this.bundle = undefined;
-  console.log('lone: extracting');
   this.extract(function(err, self){
-    console.log('lone: extracted', arguments);
     if(err) return fn(err);
-    console.log('lone: reading package.json', arguments);
     fs.readFile(path.resolve(self.dest + '/package.json'), 'utf-8', function(err, data){
       if(err) return fn(err);
 
       self.pkg = JSON.parse(data);
-      console.log('lone: package data', self.pkg);
-      console.log('lone: argv[1] is', process.argv[1]);
       if (process.argv[1] == 'debug') {
         // Start the debugger agent
-        console.log('lone: starting debugger');
         var d = require('_debugger');
         d.start();
       } else if (process.argv[1]) {
@@ -53,7 +36,6 @@ function Lone(src, fn){
         var path = require('path');
         process.argv[1] = path.resolve(process.argv[1]);
         if (process.env.NODE_UNIQUE_ID) {
-          console.log('lone: setup cluster worker');
           var cluster = require('cluster');
           cluster._setupWorker();
           delete process.env.NODE_UNIQUE_ID;
@@ -65,22 +47,19 @@ function Lone(src, fn){
           var debugTimeout = +process.env.NODE_DEBUG_TIMEOUT || 50;
           return setTimeout(main, debugTimeout);
         }
-        return main();
+        return main(self);
       } else {
-        main();
+        main(self);
       }
-      console.log('lone: calling _tickCallback');
       process._tickCallback();
       fn(null, self);
     });
   });
 }
 
-function main(){
+function main(self){
   var Module = require('module'),
     src = process.argv[1] ? process.argv[1] : path.resolve(self.dest, self.pkg.bin[self.pkg.name]);
-
-  console.log('lone: loading module', src);
   try{
     Module._load(src, null, true);
   }
@@ -116,7 +95,6 @@ Lone.prototype.extract = function(fn){
       if(chunks.length === 0){
         return fn(new Error('lone: no bundle embedded in ' + self.src + '.  0 zip chunks found in binary.'));
       }
-      console.log('lone: extracting bundle to', self.dest);
       self.bundle = Buffer.concat(chunks);
       new AdmZip(self.bundle).extractAllTo(self.dest, true);
       fn(null, self);
@@ -128,7 +106,6 @@ if(embedded){
     if(err){
       return console.error('lone: failed to extract', err);
     }
-    console.log('lone: app loaded');
   });
 }
 else {
