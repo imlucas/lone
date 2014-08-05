86c5bb733467e61dfd39503c1e4d5a6290dffea7
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-08-05 11:00:39 -0400

    fix regression in src not being set properly

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var async = require('async'),
-  lone = require('./lib');
+  lone = require('./lib'),
+  config = require('./lib/config');
 
 module.exports = function(opts, fn){
-  async.series([lone.configure.bind(null, opts), lone.bundle, lone.compile], fn);
+  async.series([lone.configure.bind(null, opts), lone.bundle, lone.compile], function(err){
+    return fn(err, config);
+  });
 };
 module.exports.options = lone.options;
--- test/hello.test.js
+++ test/hello.test.js
@@ -1,40 +1,14 @@
 var assert = require('assert'),
-  fs = require('fs'),
   child_process = require('child_process'),
   lone = require('../');
 
 describe('hello', function(){
-  var config = {
-    src: __dirname + '/fixtures/hello',
-    node: '0.10.29'
-  };
-
-  it('should create a manifest', function(done){
-    lone.manifest(config, function(err, res){
-      if(err) return done(err);
-
-      config = res;
-      assert.deepEqual(config.manifest.files, ['index.js', 'package.json']);
-      done();
-    });
-  });
-
-  it('should create a zip file', function(done){
-    lone.zip(config, function(err, config){
-      if(err) return done(err);
-
-      assert(fs.existsSync(config.bundle));
-
-      done();
-    });
-  });
-
   it('should deliver a runnable executable', function(done){
-    lone(config, function(err, res){
+    lone({src: __dirname + '/fixtures/hello', node: '0.10.29'}, function(err, res){
       if(err) return done(err);
 
-      config = res;
-      child_process.exec(config.out, function(err, stdout){
+      assert.equal(res.src, __dirname + '/fixtures/hello');
+      child_process.exec(res.out, function(err, stdout){
         if(err) return done(err);
         assert.equal(stdout.toString().replace('\n', ''), 'hello');
         done();
