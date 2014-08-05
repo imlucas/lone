15df939cf34639c33a4dec36e0cc562240aa33e6
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-08-05 07:39:31 -0400

    [#5] start pulling out derp helpers

--- bin/lone.js
+++ bin/lone.js
@@ -3,25 +3,16 @@
 var fs = require('fs'),
   lone = require('../'),
   yargs = require('yargs').usage([
-  'Turn your node.js app into a single file executable.',
-  'Usage: lone [action]\n',
-  'where [action] is any one of: ' + lone.actions.join(', ')
-].join('\n'))
-.options(lone.options)
-.demand(0);
+    'Turn your node.js app into a single file executable.',
+    'Usage: lone'
+  ].join('\n'))
+  .options(lone.options);
 
 if(yargs.argv.h) return yargs.showHelp();
 
 if(yargs.argv.setup) return console.error(fs.readFileSync(__dirname + '/setup', 'utf-8'));
 
-var action = yargs.argv._[0] || 'all';
-
-if(lone.actions.indexOf(action) === -1){
-  console.error('unknown action `'+action+'`');
-  return yargs.showHelp();
-}
-
-lone[action](yargs.argv, function(err, conf){
+lone(yargs.argv, function(err, conf){
   if(err) return console.error(':(', err.message) && process.exit(1);
   console.log(conf.out);
 });
--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-var derp = require('./lib/derp'),
+var async = require('async'),
   lone = require('./lib');
 
 module.exports = function(opts, fn){
-  var steps = [lone.configure(opts), lone.bundle, lone.compile];
-  steps.push(fn);
-  derp.sync.apply(derp, steps);
+  async.series([lone.configure.bind(null, opts), lone.bundle, lone.compile], fn);
 };
-module.exports.all = module.exports;
-
-module.exports.manifest = function(opts, fn){
-  derp.sync(lone.configure(opts), lone.bundle.manifest, fn);
-};
-
-module.exports.zip = function(opts, fn){
-  derp.sync(lone.configure(opts), lone.bundle.manifest, lone.bundle.zip, fn);
-};
-
-module.exports.bundle = function(opts, fn){
-  derp.sync(lone.configure(opts), lone.bundle, fn);
-};
-
-module.exports.compile = function(opts, fn){
-  derp.sync(lone.configure(opts), lone.compile, fn);
-};
-
-module.exports.embed = function(opts, fn){
-  derp.sync(lone.configure(opts), lone.compile.embed, fn);
-};
-
-module.exports.download = function(opts, fn){
-  derp.sync(lone.configure(opts), lone.compile.download, fn);
-};
-
-module.exports.make = function(opts, fn){
-  derp.sync(lone.configure(opts), lone.compile.make, fn);
-};
-
-module.exports.actions = Object.keys(module.exports);
 module.exports.options = lone.options;
